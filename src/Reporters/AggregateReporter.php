d2a4b9ac95ab0110cc7493074aae5e7cfa42d5a1
Taka Oyama <toyama4649@hotmail.com>
2022-07-28 00:14:13 +0900

    add AggregateReporter

---
+++ src/Reporters/AggregateReporter.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Kirameki\Exception\Reporters;
+
+use Closure;
+use Throwable;
+
+class AggregateReporter implements Reporter
+{
+    /**
+     * @param array<Reporter|Closure(): Reporter> $reporters
+     */
+    public function __construct(protected array $reporters)
+    {
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return void
+     */
+    public function report(Throwable $exception): void
+    {
+        foreach ($this->reporters as $index => $reporter) {
+            if ($reporter instanceof Closure) {
+                $reporter = $this->reporters[$index] = $reporter();
+            }
+            $reporter->report($exception);
+        }
+    }
+}
